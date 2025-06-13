12c32648d916135e5c35eb1e3ca7f1e5d20c6ea2
Aaron Sanders <aaron.sanders@grafana.com>
2025-06-13 09:49:08 -0700

    Vibe gh pages commit

---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/min-hourglass/', // Replace 'min-hourglass' with your repository name
+}) 
